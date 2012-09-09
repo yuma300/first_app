9be8edb20e3dc13c64443fe7b7a3180dc69d33fc
yuma300 <ywakimoto2s@gmail.com>
2012-09-09 11:50:05 +0900

    Chap8.2まで完了。コミット漏れのコミット

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe User do
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
+end
