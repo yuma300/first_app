d884c52b631018a887ffe49c4b0b93df8049022b
yuma300 <ywakimoto2s@gmail.com>
2012-09-11 00:43:52 +0900

    Chap9完了。Listing 9.46のapp/controllers/users_controller.rbで redirect_to users_urlとやっているのでListing 9.47ではroot_pathでは なくてusers_pathにすべきと思われ。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
@@ -32,6 +32,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
   private
     def signed_in_user
      unless signed_in?
@@ -43,5 +48,8 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120908173914) do
+ActiveRecord::Schema.define(:version => 20120910143650) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -23,13 +23,14 @@ ActiveRecord::Schema.define(:version => 20120908173914) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
     t.string   "password_digest"
     t.string   "password"
     t.string   "add_password_confirmation_to_users"
     t.string   "password_confirmation"
     t.string   "remember_token"
+    t.boolean  "admin",                              :default => false
   end
 
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
